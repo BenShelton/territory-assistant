71be2c444d7d7a41c93754f44e6d983a667bbecd
BenShelton <bensheltonjones@gmail.com>
2019-11-01 13:34:27 +0000

    style(eslint): add custom linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,82 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
+    'plugin:@typescript-eslint/recommended',
     '@vue/standard',
     '@vue/typescript'
   ],
   rules: {
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: {
+        max: 1,
+        allowFirstLine: false
+      }
+    }],
+    'vue/order-in-components': ['error', {
+      order: [
+        'el',
+        'name',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        'extends',
+        ['components', 'directives', 'filters'],
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'LIFECYCLE_HOOKS',
+        'data',
+        'computed',
+        'watch',
+        'methods',
+        ['template', 'render'],
+        'renderError'
+      ]
+    }],
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': 'error',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase', {
+      registeredComponentsOnly: true
+    }],
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': ['error', {
+      extensions: ['vue'],
+      shouldMatchCase: true
+    }],
+    'vue/no-boolean-default': 'error',
+    'vue/no-restricted-syntax': 'error',
+    'vue/object-curly-spacing': ['error', 'always'],
+    'vue/space-infix-ops': 'error',
+    'vue/space-unary-ops': 'error',
+    'vue/v-on-function-call': 'error',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/interface-name-prefix': ['error', 'always'],
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none',
+        requireLast: true
+      },
+      singleline: {
+        delimiter: 'comma'
+      }
+    }],
+    '@typescript-eslint/explicit-function-return-type': ['error', {
+      allowExpressions: true,
+      allowTypedFunctionExpressions: true,
+      allowHigherOrderFunctions: true
+    }],
+    '@typescript-eslint/explicit-member-accessibility': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
+  parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser'
   },
@@ -23,6 +91,9 @@ module.exports = {
       ],
       env: {
         jest: true
+      },
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off'
       }
     }
   ]
