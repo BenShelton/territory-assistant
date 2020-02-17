f477e8e33639191f2872bd9f0e408b932f9c01e2
BenShelton <bensheltonjones@gmail.com>
2020-02-17 22:51:12 +0000

    feat(maps): add placeholder page for map list

--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -1,16 +1,46 @@
 <template>
   <v-container>
-    <h1 class="headline">
+    <h1 class="headline mb-3">
       Maps
     </h1>
+    <v-btn color="primary" @click="onAdd">
+      <v-icon left>
+        mdi-plus
+      </v-icon>
+      ADD NEW MAP
+    </v-btn>
+    <v-row wrap>
+      <v-col>
+        <MapPreview
+          v-for="map of maps"
+          :key="map._id"
+          :map="map"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-//
+
+import store from '@/store'
+
+import { IMap } from 'types'
 
 export default Vue.extend({
-  name: 'Maps'
+  name: 'Maps',
+
+  computed: {
+    maps (): IMap[] {
+      return store.state.maps.list
+    }
+  },
+
+  methods: {
+    onAdd (): void {
+      this.$router.push('/maps/add')
+    }
+  }
 })
 </script>
